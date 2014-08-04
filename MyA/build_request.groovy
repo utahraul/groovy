e12d0041d572aa6c65d3fab2e0659ccdc0be31df
Raúl <raul.hernandez@testeandosoftware.com>
2014-08-04 12:31:55 +0200

    New build_request.groovy added

--- MyA/build_request.groovy
+++ MyA/build_request.groovy
@@ -1,23 +1,50 @@
-def properties = (com.eviware.soapui.impl.wsdl.teststeps.WsdlPropertiesTestStep) context.getTestCase().getTestStepByName("Properties");
-def random = new Random(new Date().getTime())
-def email = ("jose" + Math.abs(random.nextInt() % 10000) + "@gmail.com")
-log.info email
+class buildRequest {
+
+    static String main(args) {
+    
+    //def properties = (com.eviware.soapui.impl.wsdl.teststeps.WsdlPropertiesTestStep) context.getTestCase().getTestStepByName("Properties");
+    // generate new email
+    def random = new Random(new Date().getTime())
+    def email = ("test" + Math.abs(random.nextInt() % 10000) + "@myaccount.com"); 
+    // get password, name, surname1 and surname2 and dninumber property from TestCase Properties
+    def password = "password"; 
+    def name = "name";
+    def surname1 = "surname1";
+    def surname2 = "surname2";
+    def dninumber = "dninumber";
+    //properties.setPropertyValue("email", email);
+    //properties.saveProperties("email");
+        //generate date ISO8601 for birthDate
+        def now = new Date()
+        //def birthDate = now.format("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"); log.info birthDate 
+        def birthDate = now.format("yyyy-MM-dd"); //log.info birthDate 
+        //def birthDate = "1980-01-01"; log.info birthDate 
+    def gender = "";
+    def country = "";
+    def documentType = "";
+
+
 def sb = new StringBuffer()
-	.append(' {\n')
-	.append('   "document": {\n')
-	.append('     "email": "' + email +'",\n')
-	.append('     "password": "1234blaBLABLA",\n')
-	.append('     "name": "John",\n')
-	.append('     "surname1": "Smith",\n')
-	.append('     "surname2": "Smith",\n')
-	.append('     "gender": "' + context.expand('${gender}') + '",\n')
-	.append('     "birthDate": "2021-08-26T10:20:01.390Z",\n')
-	.append('     "nationality": "' + context.expand('${country}') + '",\n')
-	.append('     "countryOfResidence": "' + context.expand('${country}') + '",\n')
-	.append('     "documentType": "DNI",\n')
-	.append('     "documentNumber": "23234234X"\n')
-	.append('   }')
-	.append(' }');
-properties.setPropertyValue("request", sb.toString());
-properties.saveProperties("request");
-log.info sb
+     .append(' {\n')     
+    .append('   "document": {\n')     
+    .append('     "email": "' + email +'",\n')     
+    .append('     "password": "' + password +'",\n')     
+    .append('     "name": "' + name +'",\n')     
+    .append('     "surname1": "' + surname1 +'",\n')     
+    .append('     "surname2": "' + surname2 +'",\n')     
+    .append('     "gender": "' + gender + '",\n')     
+    .append('     "birthDate": "' + birthDate +'",\n')     
+    .append('     "nationality": "' + country + '",\n')     
+    .append('     "countryOfResidence": "' + country + '",\n')
+    .append('     "documentType": "' + documentType + '",\n')     
+    .append('     "documentNumber": "' + dninumber +'"\n')          
+    .append('   }')     
+    .append(' }');
+    
+    return (sb);
+//properties.setPropertyValue("request", sb.toString()); 
+//properties.saveProperties("request");// log.info sb
+
+    }
+
+}
